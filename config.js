4002094b8e1300ef5dfdbc6d5bae847f72a7169d
ataakha <muhammadaqibatta89@gmail.com>
2025-01-03 11:54:12 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "OV0HxCpL#pENCunWm8sZ1itoTAPPGKtRw2AVGufBQOfMFupSPSz8",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "2348078582627",
+    SESSION_ID: process.env.SESSION_ID || "aAlGQRhS#clbxv1uwsJDSZr9-XKH99I0BpcaNd5UEaUHrySKFpoE",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "923346690239",
     AUTO_VIEW_STATUS: process.env.AUTO_VIEW_STATUS || "false",
     MODE: process.env.MODE || "private",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/efeurhobo/Empire_X/main/lib/assets/empire.jpg",
     PREFIX: process.env.PREFIX || ".",
-    OWNER_REACT: process.env.OWNER_REACT || "false",
-    AUTO_REACT: process.env.AUTO_REACT || "false",
+    OWNER_REACT: process.env.OWNER_REACT || "true",
+    AUTO_REACT: process.env.AUTO_REACT || "true",
     OWNER_NAME: process.env.OWNER_NAME || "𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐞𝐦𝐩𝐢𝐫𝐞",
     BOT_NAME: process.env.BOT_NAME || "Empire_X",
     CAPTION: process.env.CAPTION || "Made By 𝐎𝐧𝐥𝐲_𝐨𝐧𝐞_🥇𝐞𝐦𝐩𝐢𝐫𝐞",
